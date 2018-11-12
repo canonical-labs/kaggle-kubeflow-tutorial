45618b3fce9ae13b09c990416bca6114bd311393
Carmine Rimi <carmine.rimi@gmail.com>
2018-11-13 05:35:12 +1100

    Update ubuntu image
    From early August to late October 2018 image.

--- gcp/compute_create.sh
+++ gcp/compute_create.sh
@@ -16,4 +16,4 @@ echo ${DIR}
 # GCP_SVC_ACCT=${GCP_SVC_ACCT:-`./get_svc_account.sh`}
 # --service-account=${GCP_SVC_ACCT}
 
-gcloud beta compute --project=${GCP_PROJECT} instances create ${GCP_INSTANCE_NAME} --zone=${GCP_ZONE} --machine-type=n1-standard-8 --subnet=default --network-tier=PREMIUM --no-restart-on-failure --maintenance-policy=TERMINATE --service-account=default  --scopes=https://www.googleapis.com/auth/cloud-platform --tags=k8s-nodeport,http-server,https-server --image=ubuntu-1804-bionic-v20180823 --image-project=ubuntu-os-cloud --boot-disk-size=200GB --boot-disk-type=pd-standard --boot-disk-device-name=kaggle-kubeflow-1
+gcloud beta compute --project=${GCP_PROJECT} instances create ${GCP_INSTANCE_NAME} --zone=${GCP_ZONE} --machine-type=n1-standard-8 --subnet=default --network-tier=PREMIUM --no-restart-on-failure --maintenance-policy=TERMINATE --service-account=default  --scopes=https://www.googleapis.com/auth/cloud-platform --tags=k8s-nodeport,http-server,https-server --image=ubuntu-1804-bionic-v20181029 --image-project=ubuntu-os-cloud --boot-disk-size=200GB --boot-disk-type=pd-standard --boot-disk-device-name=kaggle-kubeflow-1
